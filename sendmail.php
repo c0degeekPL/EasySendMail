5a8f435a02aed6aa41da7dc5e2b02a4750865c79
c0degeek <mateusz.kilinski@gmail.com>
2016-10-06 07:21:52 +0200

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -21,44 +21,44 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-	require							'PHPMailerAutoload.php';
-	require_once 				('class.phpmailer.php');
-	require_once 				('class.smtp.php');
+    require                 'PHPMailerAutoload.php';
+    require_once            ('class.phpmailer.php');
+    require_once            ('class.smtp.php');
 
-	$mail 							= new PHPMailer;
+    $mail                   = new PHPMailer;
 
 	// 01 ------------------------------------------------------------------------ UserForm Variables
 
-	$name_ 							= $_POST["name"];
-	$email_							= $_POST["email"];
-	$phone_							= $_POST["phone"];
-	$message_						= $_POST["message"];
+    $name_                  = $_POST["name"];
+    $email_                 = $_POST["email"];
+    $phone_                 = $_POST["phone"];
+    $message_               = $_POST["message"];
 
 	// --------------------------------------------------------------------------- end of 01
 
 	// 02 ------------------------------------------------------------------------ SendMail Variables
 
-	$smtp['server']  			= 'smtp.server.com';
-	$smtp['username']			= 'username';
-	$smtp['password']			= 'password123';
+    $smtp['server']         = 'smtp.server.com';
+    $smtp['username']       = 'username';
+    $smtp['password']       = 'password123';
 
-	$sendmail['from']  		= 'No-Reply';
-	$sendmail['to']    		= 'receiver_email_address';
-	$sendmail['reply_to']	= $email_;
-	$sendmail['CC']				= '';
-	$sendmail['BCC']			= '';
+    $sendmail['from']       = 'No-Reply';
+    $sendmail['to']         = 'receiver_email_address';
+    $sendmail['reply_to']   = $email_;
+    $sendmail['CC']         = '';
+    $sendmail['BCC']        = '';
 
-	$message['success']		= 'Message was sent. We will contact you soon.';
-	$message['error']			= 'There is an error while sending message. Error details: ';
-	$message['no_access'] = 'You don\'t have persmissions to view this page!';
+    $message['success']     = 'Message was sent. We will contact you soon.';
+    $message['error']       = 'There is an error while sending message. Error details: ';
+    $message['no_access']   = 'You don\'t have persmissions to view this page!';
 
 	// 02.01 --------------------------------------------------------------------- Mail Body Content
 
-	$webpage_used					= 'yourweb.com';
-	$sendmail['subject']	= 'New message from ' . $webpage_used . ' webpage from ' . $name_;
-	$sendmail['body']			= 'You have a message from ' . $webpage_used . ' webpage from ' . $name_;
-	$sendmail['body']  	 .= '<br/><strong>Message: </strong><br/>' . $message_;
-	$sendmail['body']  	 .= '<br/><br/>Contact: ' . $name_ . ', phone: ' . $phone_;
+    $webpage_used           = 'yourweb.com';
+    $sendmail['subject']    = 'New message from ' . $webpage_used . ' webpage from ' . $name_;
+    $sendmail['body']       = 'You have a message from ' . $webpage_used . ' webpage from ' . $name_;
+    $sendmail['body']      .= '<br/><strong>Message: </strong><br/>' . $message_;
+    $sendmail['body']  	   .= '<br/><br/>Contact: ' . $name_ . ', phone: ' . $phone_;
 
 	// --------------------------------------------------------------------------- end of 02.01
 
@@ -66,27 +66,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	// 04 ------------------------------------------------------------------------ SendMail Functions
 
-	$mail->isSMTP();                              					// Set mailer to use SMTP
-	$mail->Host 					= $smtp['server']; 								// Specify main and backup SMTP servers
-	$mail->SMTPAuth 			= true;                      			// Enable SMTP authentication
-	$mail->Username 			= $smtp['username'];              // SMTP username
-	$mail->Password 			= $smtp['password'];             	// SMTP password
-	$mail->SMTPSecure 		= '';                       			// Enable TLS encryption, `ssl` also accepted
-	$mail->SMTPAutoTLS 		= false;
-	$mail->Port 					= 587;                            // TCP port to connect to
+    $mail->isSMTP();                                                // Set mailer to use SMTP
+    $mail->Host             = $smtp['server'];                      // Specify main and backup SMTP servers
+    $mail->SMTPAuth         = true;                                 // Enable SMTP authentication
+    $mail->Username         = $smtp['username'];                    // SMTP username
+    $mail->Password         = $smtp['password'];                    // SMTP password
+    $mail->SMTPSecure       = '';                                   // Enable TLS encryption, `ssl` also accepted
+    $mail->SMTPAutoTLS      = false;
+    $mail->Port             = 587;                                  // TCP port to connect to
 
-	$mail->setFrom($smtp['username'], $sendmail['from']);
-	$mail->addAddress($sendmail['to']);								    	// Add a recipient
-	if ($sendmail['reply_to'] != '')	$mail->addReplyTo($sendmail['reply_to']);
-	if ($sendmail['CC'] != '')				$mail->addCC($sendmail['CC']);
-	if ($sendmail['BCC'] != '')				$mail->addBCC($sendmail['BCC']);
+    $mail->setFrom($smtp['username'], $sendmail['from']);
+    $mail->addAddress($sendmail['to']);                             // Add a recipient
+    if ($sendmail['reply_to'] != '')        $mail->addReplyTo($sendmail['reply_to']);
+    if ($sendmail['CC'] != '')              $mail->addCC($sendmail['CC']);
+    if ($sendmail['BCC'] != '')             $mail->addBCC($sendmail['BCC']);
 
-	$mail->isHTML(true);                          					// Set email format to HTML
-	$mail->setLanguage('pl', 'languages/');									// Error Language
-	$mail->CharSet 				= 'utf-8';
+    $mail->isHTML(true);                                            // Set email format to HTML
+    $mail->setLanguage('pl', 'languages/');                         // Error Language
+    $mail->CharSet          = 'utf-8';
 
-	$mail->Subject 				= $sendmail['subject'];
-	$mail->Body    				= $sendmail['body'];
+	$mail->Subject          = $sendmail['subject'];
+	$mail->Body             = $sendmail['body'];
 
 	if(!$mail->send()) {
 		http_response_code(500);
